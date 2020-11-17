b0078acfac798f46dfe4c354a781d40f61f4e4c5
Gonzalo Amadio <gonzalo.amadio@onapsis.com>
2020-11-17 12:57:50 -0300

    Add how to exec remote passing password

--- execRemote.sh
+++ execRemote.sh
@@ -48,3 +48,15 @@ Then if you have ssh key in place. Just use one of the above methors:
 
     ssh usr@remote_host 'bash /home/user/route.sh' # this one should work
     ssh usr@remote_host 'sudo nohup bash -c "/home/user/route.sh > /dev/null 2>&1 &"'
+
+##### PASANDO PASSWORD
+#!/bin/bash
+
+sshpass -p angrys370 ssh superuser@10.85.1.60 << EOF
+svctask startrcconsistgrp -force 0
+svctask startrcconsistgrp -force 1
+svctask startrcconsistgrp -force 2
+exit
+EOF
+
+
--- loginSsh.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sshpass -p angrys370 ssh superuser@10.85.1.60 << EOF
-svctask startrcconsistgrp -force 0
-svctask startrcconsistgrp -force 1
-svctask startrcconsistgrp -force 2
-exit
-EOF
